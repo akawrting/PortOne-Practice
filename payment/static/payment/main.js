b53ca302959e1746fb0fe460233a1259b98e93f6
akawrting <simpilgione@gmail.com>
2025-05-23 17:53:38 +0900

    결제완료, 결제실패 리다이렉트 추가
    결제 완료시 merchant_uid값 수정

--- payment/static/payment/main.js
+++ payment/static/payment/main.js
@@ -1,14 +1,26 @@
 IMP.init("imp54322068");
 
 const button = document.querySelector("button");
+const merchantUid = `ORD-${Date.now()}`;
 
 const onClickPay = async () => {
     IMP.request_pay({
         pg: "kakaopay",
         pay_method: "card",
-        amount: "9999999",
-        name: "저는 님을 벗겨먹으러 왔습니다",
-        merchant_uid: "ORD20250523-000003"
+        amount: "100",
+        name: "저는 님을 벗겨먹으러 온 사람입니다",
+        merchant_uid: merchantUid,
+    }, 
+    function(response){
+        if(response.success){
+            alert("결제가 완료되었습니다!");
+            window.location.href = `http://127.0.0.1:8000/payment/payment_success?paid_amount=${response.paid_amount}&merchant_uid=${response.merchant_uid}`;
+        }
+        else {
+            alert("결제에 실패하였습니다.");
+            window.location.href = "http://127.0.0.1:8000/payment/payment_failed";
+        }
+
     });
 
 };
--- payment/urls.py
+++ payment/urls.py
@@ -6,5 +6,6 @@ app_name = "payment"
 urlpatterns = [
     
     path("", views.index, name="index"),
-    
+    path("payment_success/", views.payment_success, name="payment_success"),
+    path("payment_failed/", views.payment_failed, name="payment_failed"),
 ]
