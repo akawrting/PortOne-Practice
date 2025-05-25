19ef8f7976c583bf252c6540640e1df54df5377d
akawrting <simpilgione@gmail.com>
2025-05-25 15:50:01 +0900

    결제금액 views.py에서 변수로 받을 수 있게 수정,
    결제 완료 페이지 깨지던 버그 수정

--- payment/static/payment/main.js
+++ payment/static/payment/main.js
@@ -1,23 +1,22 @@
 IMP.init("imp54322068");
 
 const button = document.querySelector("button");
-const merchantUid = `ORD-${Date.now()}`;
+const merchantUid = new Date().getTime();
+const pay_amount = document.getElementById('pay-amount').dataset.amount;
 
 const onClickPay = async () => {
     IMP.request_pay({
         pg: "kakaopay",
         pay_method: "card",
-        amount: "100",
+        amount: pay_amount,
         name: "저는 님을 벗겨먹으러 온 사람입니다",
         merchant_uid: merchantUid,
     }, 
     function(response){
         if(response.success){
-            alert("결제가 완료되었습니다!");
             window.location.href = `http://127.0.0.1:8000/payment/payment_success?paid_amount=${response.paid_amount}&merchant_uid=${response.merchant_uid}`;
         }
         else {
-            alert("결제에 실패하였습니다.");
             window.location.href = "http://127.0.0.1:8000/payment/payment_failed";
         }
 
--- payment/views.py
+++ payment/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-# Create your views here.
 
 def index(request):
-    return render(request, 'payment/index.html')
+    #여기에 가격 들어가게 하면 됨
+    pay_amount = 1000
+    return render(request, 'payment/index.html' , {"pay_amount": pay_amount})
 
 def payment_success(request):
     paid_amount = request.GET.get("paid_amount", '0')
     merchant_uid = request.GET.get("merchant_uid", '0')
-    return render(request, 'payment/payment_success.html', {"paid_amount": paid_amount}, {"merchant_uid": merchant_uid})
+    return render(request, 'payment/payment_success.html', {"paid_amount": paid_amount, "merchant_uid": merchant_uid})
 
 def payment_failed(request):
     return render(request, 'payment/payment_failed.html')
